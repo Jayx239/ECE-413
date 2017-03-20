10d2d4b58d10640a67082c57ef62421b8291c46b
Jason Gallagher <jpg77@drexel.edu>
2017-03-19 20:57:53 -0400

    Optimized code, fixed timing code. Now acheiving speedups for optimized code.

--- CUDA_2D/2Dconvolution.cu
+++ CUDA_2D/2Dconvolution.cu
@@ -19,55 +19,87 @@ void CopyToDeviceMatrix(Matrix Mdevice, const Matrix Mhost);
 void CopyFromDeviceMatrix(Matrix Mhost, const Matrix Mdevice);
 void FreeDeviceMatrix(Matrix* M);
 void FreeMatrix(Matrix* M);
-void ConvolutionOnDevice(const Matrix M, const Matrix N, Matrix P);
+float ConvolutionOnDevice(const Matrix M, const Matrix N, Matrix P);
+float OptimizedConvolutionOnDevice(const Matrix M, const Matrix N, Matrix P);
 int checkResults(float *, float *, int, float);
 
+__constant__ float cM[KERNEL_SIZE*KERNEL_SIZE];
 
+__global__ void OptimizedConvolutionKernel(Matrix N, Matrix P)
+{
+    int j = blockIdx.x * blockDim.x + threadIdx.x;
+    int i = blockIdx.y * blockDim.y + threadIdx.y;
+    double sum = 0;
+
+    unsigned int mBeg = i < 2 ? 2-i: 0;
+    unsigned int mEnd = i>(MATRIX_SIZE-3) ? MATRIX_SIZE-i+2  : 5;
+    unsigned int nBeg = j < 2 ? 2-j : 0;
+    unsigned int nEnd = j > (MATRIX_SIZE-3) ? MATRIX_SIZE-j+2  : 5;
+
+    for(int m=mBeg; m < mEnd; m++)
+        for(int n=nBeg; n < nEnd; n++)
+            sum += cM[KERNEL_SIZE*m + n] * N.elements[MATRIX_SIZE*(i + m-2) + ((j + n-2))];
+
+    P.elements[ MATRIX_SIZE*i + j] = (float)sum;
+
+}
+
+//__constant__ float cN[MATRIX_SIZE*MATRIX_SIZE];
 int main(int argc, char** argv) 
 {
 
 	Matrix  A;
 	Matrix  B;
 	Matrix  C;
-	
+    Matrix C_opt;	
+
 	srand(time(NULL));
-    struct timeval start1,stop1,start2,stop2;
+    struct timeval start1,stop1,start2,stop2,start3,stop3;
 
 	// Allocate and initialize the matrices
 	A  = AllocateMatrix(KERNEL_SIZE, KERNEL_SIZE, 1);
 	B  = AllocateMatrix(MATRIX_SIZE, MATRIX_SIZE, 1);
 	C  = AllocateMatrix(B.height, B.width, 0);
+    C_opt = AllocateMatrix(B.height, B.width, 0);
 
-    gettimeofday(&start1,NULL);    
    /* Convolve matrix B with matrix A on the CPU. */
-   Matrix reference = AllocateMatrix(C.height, C.width, 0);
+    gettimeofday(&start1,NULL);   
+    Matrix reference = AllocateMatrix(C.height, C.width, 0);
    computeGold(reference.elements, A.elements, B.elements, B.height, B.width);
     gettimeofday(&stop1,NULL);   
 	
-    gettimeofday(&start2,NULL);
     /* Convolve matrix B with matrix A on the device. */
-   ConvolutionOnDevice(A, B, C);
+    float ktime,optktime;
+    /* Un-optimized */
+    gettimeofday(&start2,NULL);
+    ktime = ConvolutionOnDevice(A, B, C);
     gettimeofday(&stop2,NULL);
-    
+    /* Optimized */
+    gettimeofday(&start3,NULL);
+    optktime = OptimizedConvolutionOnDevice(A,B,C_opt);
+    gettimeofday(&stop3,NULL);    
+
     float cpuruntime = (float)(stop1.tv_sec - start1.tv_sec + (stop1.tv_usec - start1.tv_usec)/(float)1000000);
-    float devruntime = (float)(stop2.tv_sec - start2.tv_sec + (stop2.tv_usec - start2.tv_usec)/(float)1000000);
     
-    printf("CPU run time = %0.2f s\nDevice run time = %0.2f s\n Speedup: %0.2f x\n",cpuruntime,devruntime,cpuruntime/devruntime);
+    printf("CPU run time = %0.2f s\nDevice run time = %0.2f s || Speedup: %0.2f x\nOptimized device run time = %0.2f s || Speedup: %0.2f x\nParallel speedup: %0.2f x\n\n",cpuruntime,ktime,cpuruntime/ktime,optktime,cpuruntime/optktime,ktime/optktime);
+
    /* Check if the device result is equivalent to the expected solution. */
     int num_elements = C.height * C.width;
 	int status = checkResults(reference.elements, C.elements, num_elements, 0.001f);
-	printf("Test %s\n", (1 == status) ? "PASSED" : "FAILED");
+	printf("Test Unoptimized %s\n\n", (1 == status) ? "PASSED" : "FAILED");
+    
+    status = checkResults(reference.elements, C_opt.elements, num_elements, 0.001f);
+    printf("Test Optimized %s\n", (1 == status) ? "PASSED" : "FAILED");
 
    // Free matrices
    FreeMatrix(&A);
    FreeMatrix(&B);
    FreeMatrix(&C);
-	
+   FreeMatrix(&C_opt);	
    return 0;
 }
 
-
-void ConvolutionOnDevice(const Matrix M, const Matrix N, Matrix P)
+float ConvolutionOnDevice(const Matrix M, const Matrix N, Matrix P)
 {
     struct timeval start1, stop1, start2, stop2;
 
@@ -103,9 +135,56 @@ void ConvolutionOnDevice(const Matrix M, const Matrix N, Matrix P)
     
     gettimeofday(&stop1,NULL);
     
+    /* Timing calculations */
     float kernel_runtime = (float)(stop2.tv_sec - start2.tv_sec + (stop2.tv_usec - start2.tv_usec)/(float)1000000);
     float overhead = (float)(stop1.tv_sec - start1.tv_sec + (stop1.tv_usec - start1.tv_usec)/(float)1000000) - kernel_runtime;
-    printf("Kernel Runtime: %f\nOverhead: %f\n Percent Overhead: %f\n",kernel_runtime,overhead,(100*overhead)/(overhead + kernel_runtime));
+
+    printf("Unoptimized Kernel Runtime: %f\nOverhead: %f\nPercent Overhead: %f\n\n",kernel_runtime,overhead,(100*overhead)/(overhead + kernel_runtime));
+    
+    return overhead + kernel_runtime;
+}
+
+float OptimizedConvolutionOnDevice(const Matrix M, const Matrix N, Matrix P)
+{
+    struct timeval start1, stop1, start2, stop2;
+
+    gettimeofday(&start1,NULL);
+    // Load M and N to the device
+    cudaMemcpyToSymbol(cM,M.elements,KERNEL_SIZE*KERNEL_SIZE*sizeof(float));
+    Matrix Nd = AllocateDeviceMatrix(N);
+    CopyToDeviceMatrix(Nd, N);
+
+    // Allocate P on the device
+    Matrix Pd = AllocateDeviceMatrix(P);
+    CopyToDeviceMatrix(Pd, P); // Clear memory
+
+    // Setup the execution configuration
+    dim3 dimBlock(THREAD_BLOCK_SIZE, THREAD_BLOCK_SIZE);
+    dim3 dimGrid(N.width/dimBlock.x,N.height/dimBlock.y);
+
+    gettimeofday(&start2,NULL);
+    // Launch the device computation threads!
+    OptimizedConvolutionKernel<<<dimGrid,dimBlock>>>(Nd,Pd);
+    cudaThreadSynchronize();
+
+    gettimeofday(&stop2,NULL);
+    
+    // Read P from the device
+    CopyFromDeviceMatrix(P, Pd); 
+    
+    // Free device matrices
+    FreeDeviceMatrix(&Nd);
+    FreeDeviceMatrix(&Pd);
+    
+    gettimeofday(&stop1,NULL);
+    
+    /* Timing calculations */
+    float kernel_runtime = (float)(stop2.tv_sec - start2.tv_sec + (stop2.tv_usec - start2.tv_usec)/(float)1000000);
+    float overhead = (float)(stop1.tv_sec - start1.tv_sec + (stop1.tv_usec - start1.tv_usec)/(float)1000000) - kernel_runtime;
+    
+    printf("Optimized Kernel Runtime: %f\nOverhead: %f\nPercent Overhead: %f\n\n",kernel_runtime,overhead,(100*overhead)/(overhead + kernel_runtime));
+    /* Return run time */
+    return overhead + kernel_runtime;
 }
 
 // Allocate a device matrix of same size as M.
